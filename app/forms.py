53d622729f098db73ec4b1309141c2c9fea9b82d
sareil <sareil.lee@gmail.com>
2015-08-26 16:38:13 +0800

    150826-1638-checkpoint

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    loginid = StringField('loginid', validators=[DataRequired()])
+    logincode = PasswordField('logincode', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
+from .forms import LoginForm
 
+# views
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/main')
+def main():
+    return render_template("main.html",
+    title = 'Home')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+    title = 'Login',
+    form = form)
+
+# error page
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
